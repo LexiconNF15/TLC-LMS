b80f61f8bfc1ba0346fd1b8d0b6f672ea28964a6
Lena Kallgren <lkallgren@live.se>
2016-02-24 13:26:37 +0100

    fix  av Group.Model

---
+++ LearningManagementSystem/Models/Group.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LearningManagementSystem.Models
+{
+    public class Group
+    {
+        public int GroupId { get; set; }
+        public string GroupName { get; set; }
+        public string GroupDescription { get; set; }
+        public DateTime GroupStart { get; set; }
+        public DateTime? GroupEnd { get; set; }
+
+        ///navigation
+        ///
+        public virtual ICollection<Course> Courses { get; set; }
+
+        public virtual ICollection<ApplicationUser> Users { get; set; }
+
+       
+
+
+        ///
+    }
+        
+}
