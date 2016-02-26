0b5d6afbeab79b77f05e5e95aa3728f19920f4ab
Larson7 <racing_champ@hotmail.com>
2016-02-26 16:20:10 +0100

    CRUD-vyer för aktiviteter, bind activityTypeId i activities controller.

--- LearningManagementSystem/Models/Activity.cs
+++ LearningManagementSystem/Models/Activity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,11 +9,17 @@ namespace LearningManagementSystem.Models
     public class Activity
     {
         public int ActivityId { get; set; }
+         [Display (Name="Aktivitet")]
         public string ActivityName { get; set; }
+         [Display(Name = "Beskrivning")]
         public string ActivityDescription { get; set; }
+         [Display(Name = "Startdatum")]
         public DateTime ActivityStart { get; set; }
+         [Display(Name = "Slutdatum")]
         public DateTime? ActivityEnd { get; set; }
+         [Display(Name = "Id för kurs")]
         public int CourseId { get; set; }
+         [Display(Name = "Id för aktivitetstyp")]
         public int ActivityTypeId { get; set; }
        
         public virtual ActivityType ActivityType { get; set; }
