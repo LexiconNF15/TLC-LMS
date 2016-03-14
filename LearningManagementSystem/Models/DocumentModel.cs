e644a0a5619a5afc667cbae6c80187b3add769a8
Lena Kallgren <lkallgren@live.se>
2016-03-14 10:51:17 +0100

    Dokumenthantering, Index, Create, Edit, Delete,DetailsVyn, DocumentModels DocumentController.

--- LearningManagementSystem/Models/DocumentModel.cs
+++ LearningManagementSystem/Models/DocumentModel.cs
@@ -11,20 +11,27 @@ namespace LearningManagementSystem.Models
     {
         public int DocumentId { get; set; }
         [StringLength(255)]
+        [Required]
+        [Display(Name = "Dokumentnamn")]
         public string DocumentName { get; set; }
         //public FileType FileType { get; set; }
+        [Required]
+        [Display(Name = "Beskrivning")]
         public string Description { get; set; }
+        [Display(Name = "Filnamn")]
         public string FileName { get; set; }
+        [Display(Name = "Filpath")]
         public string FilePath { get; set; }
 
         public string UploaderId { get; set; }
+        [Display(Name = "Uppladdat")]
         public DateTime UploadDate { get; set; }
-       
+
         public int? GroupId { get; set; }
         public int? CourseId { get; set; }
         public int? ActivityId { get; set; }
 
-    
+
         [ForeignKey("UploaderId")]
         public virtual ApplicationUser Uploader { get; set; }
 
