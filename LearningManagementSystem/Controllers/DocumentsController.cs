ab646d6329de9844484b2d1691f563c7b4a05daf
Cecilia <cecilia.lipfert@gmail.com>
2016-03-15 09:27:43 +0100

    Endast lärare kan komma åt dokumenten

--- LearningManagementSystem/Controllers/DocumentsController.cs
+++ LearningManagementSystem/Controllers/DocumentsController.cs
@@ -11,7 +11,7 @@ using System.IO;
 
 namespace LearningManagementSystem.Controllers
 {
-    [Authorize]
+    [Authorize(Roles="Teacher")]
     public class DocumentsController : Controller
     {
         private ApplicationDbContext db = new ApplicationDbContext();
